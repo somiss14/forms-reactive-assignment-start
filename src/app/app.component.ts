6efbd248a63e4cb6fc055a00c9e684d177d41660
maciek@arch <mcyran1996@gmail.com>
2023-12-12 16:41:49 +0100

    assignment done, not checked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Observable} from "rxjs";
+import {resolve} from "@angular/compiler-cli/src/ngtsc/file_system";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  stateForm: FormGroup;
+  projectStatuses = ['Stable', 'Critical', 'Finished'];
+  forbiddenName = 'Test';
+
+  ngOnInit() {
+    this.stateForm = new FormGroup({
+      // 'name': new FormControl(null, [Validators.required, this.forbiddenNameChecker.bind(this)]),
+      'name': new FormControl(null, [Validators.required], this.asyncForbiddenNameChecker),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl(null)
+    });
+
+    this.stateForm.statusChanges.subscribe(
+      (value) => {
+        console.log(value);
+      }
+    )
+  }
+
+  forbiddenNameChecker(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenName.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true}
+    }
+    return null;
+  }
+
+  asyncForbiddenNameChecker(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({'nameIsForbidden': true})
+        } else {
+          resolve(null)
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
+  onSubmit() {
+    console.log(this.stateForm);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -9,10 +9,11 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
